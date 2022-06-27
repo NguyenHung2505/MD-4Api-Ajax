3373349c11bf3d6da1d54f47d71670bfe9e30299
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-06-27 15:40:15 +0700

    validate

--- src/main/java/com/example/md4ajax/controller/ProductController.java
+++ src/main/java/com/example/md4ajax/controller/ProductController.java
@@ -7,7 +7,13 @@ import com.example.md4ajax.service.product.ProductIMPL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -16,31 +22,45 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
     @Autowired
     ProductIMPL productService;
+
+
     @GetMapping
-    public ResponseEntity<Iterable<Product>> findALLProduct(){
+    public ResponseEntity<Iterable<Product>> findALLProduct() {
         return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
     }
+
     @GetMapping("/order-by-price")
-    public ResponseEntity<Iterable<Product>> findAllByOrderByPrice(){
+    public ResponseEntity<Iterable<Product>> findAllByOrderByPrice() {
         return new ResponseEntity<>(productService.findAllByOrderByPrice(), HttpStatus.OK);
     }
 
     @GetMapping("/price-between")
-    public ResponseEntity<Iterable<Product>> findAllByPriceBetween(@RequestParam int from ,@RequestParam int to ){
-        return new ResponseEntity<>(productService.findAllByPriceBetween(from,to), HttpStatus.OK);
+    public ResponseEntity<Iterable<Product>> findAllByPriceBetween(@RequestParam int from, @RequestParam int to) {
+        return new ResponseEntity<>(productService.findAllByPriceBetween(from, to), HttpStatus.OK);
     }
 
     @GetMapping("/search-name")
-    public ResponseEntity<Iterable<Product>> findAllByNameContaining(String name){
+    public ResponseEntity<Iterable<Product>> findAllByNameContaining(String name) {
         return new ResponseEntity<>(productService.findAllByNameContaining(name), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> saveProduct(@Valid @RequestBody Product product) {
         productService.save(product);
-        return new ResponseEntity<>( HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
 
 
 }
--- src/main/java/com/example/md4ajax/model/Product.java
+++ src/main/java/com/example/md4ajax/model/Product.java
@@ -1,6 +1,12 @@
 package com.example.md4ajax.model;
 
+import org.springframework.stereotype.Component;
+
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "product")
@@ -8,7 +14,10 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "nhap ten vaoooo nhanh len")
     private String name;
+    @Max(value = 3000, message = "nhap qua ui")
+    @Min(value = 0 ,message = "ko nhap so am")
     private int price;
 
     @ManyToOne
